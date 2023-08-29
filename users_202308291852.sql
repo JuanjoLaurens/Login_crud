694bf1f6dfed4f1488b902afe0ef82b6248595ea
juan.laurens <juanlaurens22@gmail.com>
2023-08-29 18:53:27 -0500

    inserts users table

---
+++ users_202308291852.sql
@@ -0,0 +1,5 @@
+INSERT INTO login_crud.users (nombre,apellido,equipo,edad,email,password,created,modified) VALUES
+	 ('Juan','Laurens','Medellin',22,'16rusherlaurens@gmail.com','$2y$10$9l5XlkA33ZI6efx1T.dgA.sgg1C8QCgFlPEQGWbomrVUXABtYrIGa','2023-08-29 20:09:05','2023-08-29 20:09:05'),
+	 ('Maria Jose','Alzate','Nacional',19,'maria@das.com','$2y$10$3qItOZadqPJtJOjFAQhRXuMSvCz3SItWA/HjmGpSMDEqwcLXPA2RG','2023-08-29 23:05:27','2023-08-29 23:05:27'),
+	 ('Nicolle','Velez','Medellin',20,'nicol@nicol.com','$2y$10$gOHQj.N0gis3/sDl.JAJ7.JnANzlVAnJLhyzz4fH32zRmZs1hCt8y','2023-08-29 23:05:50','2023-08-29 23:05:50'),
+	 ('Valeria','Davila','America',20,'valeria@valeria.com','$2y$10$m0lujYAKSHzVVMFUR/oTfuVK/XBhIHxZdMpoSRtuqTbz7.0Nj/PYm','2023-08-29 23:11:41','2023-08-29 23:11:41');
